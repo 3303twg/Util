8063dec931bad1da133afa24136804095398baea
3303twg <3303twg@naver.com>
2026-04-21 13:19:13 +0900

    Style : [Singleton.cs]불필요한 변수 제거

--- Singleton.cs
+++ Singleton.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T _instance;
-    private static bool applicationIsQuitting = false;
 
     public static T Instance
     {
